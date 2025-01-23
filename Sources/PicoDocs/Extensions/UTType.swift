ebcd8c8c4846cbc874c49185e541307b20a08926
Rudrank Riyam <rudrankriyam@gmail.com>
2025-01-23 12:58:30 +0530

    Remove doc and docs

--- Sources/PicoDocs/Extensions/UTType.swift
+++ Sources/PicoDocs/Extensions/UTType.swift
@@ -9,21 +9,17 @@ import Foundation
 import UniformTypeIdentifiers
 
 public extension UTType {
-    
-    // Custom types
-    static let doc = UTType(importedAs: "com.microsoft.word.doc", conformingTo: .data)
-    static let docx = UTType(importedAs: "org.openxmlformats.wordprocessingml.document", conformingTo: .xml)
-    static let xhtml = UTType(importedAs: "public.xhtml", conformingTo: .xml)
-    static let webloc = UTType(importedAs: "com.apple.web-internet-location")
+
+   // static let xhtml = UTType(importedAs: "public.xhtml", conformingTo: .xml)
+  //  static let webloc = UTType(importedAs: "com.apple.web-internet-location")
 
     /// Array of all supported documents
     static let supportedDocumentTypes = [
         .folder, .directory,
-        .webloc,
-        .doc, .docx,
+      //  .webloc, .xhtml,
         .pdf, .rtf, .rtfd, .text, .flatRTFD, .plainText, .utf8PlainText, xml,
         .spreadsheet, .commaSeparatedText,
-        .internetLocation, .internetShortcut, .url, .urlBookmarkData, .html, .xhtml,
+        .internetLocation, .internetShortcut, .url, .urlBookmarkData, .html,
         .sourceCode, .json, .objectiveCSource, .phpScript, .perlScript, .shellScript, .script, .javaScript, .pythonScript, .assemblyLanguageSource,
         .emailMessage, .spreadsheet,
     ]
